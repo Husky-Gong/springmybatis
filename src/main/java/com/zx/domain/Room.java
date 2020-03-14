df7828494abbfa15b09a5c47819dcb5bfae4b2a8
ZexiGong <gong.ze@husky.neu.edu>
2020-03-14 23:31:11 +0800

    update application.xml

---
+++ src/main/java/com/zx/domain/Room.java
@@ -0,0 +1,53 @@
+package com.zx.domain;
+
+public class Room {
+    private Integer id;
+
+    private Integer type;
+
+    private Integer price;
+
+    private String info;
+
+    private Integer hotelId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+}
